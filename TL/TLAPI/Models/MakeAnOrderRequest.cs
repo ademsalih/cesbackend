5c8799234c0a2a54c90d2f2a559d0f77746a4cc2
Maciej Koćmierowski <maciej.kocmierowski@gmail.com>
2021-10-14 20:46:54 +0200

    Add order service

---
+++ TL/TLAPI/Models/MakeAnOrderRequest.cs
@@ -0,0 +1,11 @@
+﻿using TelstarLogistics.DataAccess.Classes;
+
+namespace TLAPI.Models
+{
+    public class MakeAnOrderRequest
+    {
+        public Customer Customer { get; set; }
+        public Parcel Parcel { get; set; }
+        public Route Route { get; set; }
+    }
+}
---
+++ TL/TLAPI/Models/MakeAnOrderResponse.cs
@@ -0,0 +1,10 @@
+﻿using TelstarLogistics.DataAccess.Classes;
+
+namespace TLAPI.Models
+{
+    public class MakeAnOrderResponse
+    {
+        public Customer Customer { get; set; }
+        public Parcel Parcel { get; set; }
+    }
+}
