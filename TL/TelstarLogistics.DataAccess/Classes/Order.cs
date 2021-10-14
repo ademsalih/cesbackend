504de03de7546fdcc0b15d810c675cab1e6c5a41
Jane Nguyen <jngu@netcompany.com>
2021-10-14 14:21:35 +0200

    Models

---
+++ TL/TelstarLogistics.DataAccess/Classes/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using TelstarLogistics.DAL.Classes;
+
+namespace TelstarLogistics.DataAccess.Classes
+{
+    public class Order
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int OrderId { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime DeliveryDate { get; set; }
+        public Employee CreatedBy { get; set; }
+        public Customer Customer { get; set; }
+        public Parcel Parcel { get; set; }
+        public Route Route { get; set; }
+    }
+}
---
+++ TL/TelstarLogistics.DataAccess/Classes/Parcel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using TelstarLogistics.DataAccess.Enum;
+
+namespace TelstarLogistics.DataAccess.Classes
+{
+    public class Parcel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ParcelId { get; set; }
+        public Type Type { get; set; }
+        public double Weight { get; set; }
+        public double Size { get; set; }
+    }
+}
